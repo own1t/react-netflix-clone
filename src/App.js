200c3eb3b9df8e8429ce117feb6f3abce2a41f83
Ryan Kim <iawal86@gmail.com>
2020-09-10 12:25:31 -0700

    created movie row

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
 
+import requests from "./requests";
+
+import Banner from "./components/Banner";
+import Row from "./components/Row";
+
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="App">
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
 }
 
 export default App;
